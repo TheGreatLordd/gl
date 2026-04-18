39cf7a921cec406eb09586d7e04ac2e08b55b7eb
Jacob <auburn300jtn@proton.me>
2026-04-17 23:59:52 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,72 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <ostream>
+#include <cstdlib>
+
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+bool gKeyWasPressed = false;
 
 int main() {
-    std::cout << "hello, world!";
+
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800,600,"goob",NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to intiialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glClearColor(0.08f, 0.85f, 0.5f, 1.0f);
+    while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        //glClearColor(0.08f, 0.85f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    bool gKeyIsPressed = glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS;
+
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+
+    } else if(gKeyIsPressed && !gKeyWasPressed) {
+        glClearColor(
+            static_cast<float>(rand()) / RAND_MAX,
+            static_cast<float>(rand()) / RAND_MAX,
+            static_cast<float>(rand()) / RAND_MAX,
+            1.0f
+        );
+    }
+
+    gKeyWasPressed = gKeyIsPressed;
 }
